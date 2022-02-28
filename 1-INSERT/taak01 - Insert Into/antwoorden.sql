5003bb1e49137f13c3605a4e5b91189adc59eb02
jayden van der bijl <2158035@talnet.nl>
2022-02-28 14:27:27 +0100

    opdracht1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,18 @@
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES ('1', 'Jeffrey', 'van den Heuvel','9A' ,'' ,'' ,'' ,'Aalsmeer' )
+
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES ('5', 'Samantha', 'van der Lip','9A' ,'Tuin 5' ,'1431LW' ,'2003-12-04' ,'Aalsmeer' )
+
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES ('6', 'Jayden', 'van der Bijl','9A' ,'Tuin 5' ,'1431LW' ,'2005-04-22' ,'Aalsmeer' )
+
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES ('7', 'Thijmen', 'van Vliet','9A' ,'schoorsteen 14' ,'1431LV' ,'2007-03-26' ,'Aalsmeer' )
+
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES ('8', 'Paulien', 'Miltenburg','9A' ,'Kas 6' ,'1431LX' ,'1973-08-12' ,'Aalsmeer' )
+
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES ('9', 'Marius', 'Restua','9A' ,'prinshendrikkade 66' ,'1234AB' ,'2000-05-15' ,'Amsterdam' )
 
