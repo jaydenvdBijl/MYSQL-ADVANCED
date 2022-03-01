867825357a360d61f39a7ee9a4a9fedc740953be
jayden van der bijl <2158035@talnet.nl>
2022-03-01 09:15:53 +0100

    opdracht2

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,10 @@
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES ('10', 'Dirk', 'Jan','9A' ,'' ,'' ,'2000-01-22' ,'Rotterdam' )
+
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES ('11', 'Ling', 'yo Long','9A' ,'' ,'' ,'1893-02-09' ,'Amsterdam' )
+
+INSERT INTO `studenten`(`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES ('12', 'Mike', 'Oxsmal','9A' ,'' ,'' ,'2015-05-29' ,'Urk' )
+
 
